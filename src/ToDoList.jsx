695d5ff6588f912f041bd3a3842a9fe93c8c050f
tuminhdiep <tuminh2307@gmail.com>
2025-04-26 20:54:26 -0400

    CRUD of todolist in React

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -18,15 +18,24 @@ function ToDoList(){
     }
 
     function deleteTask(index){
-
+        const updatedTasks = tasks.filter((_, i) => i !== index);
+        setTasks(updatedTasks);
     }
 
     function moveTaskUp(index){
-
+        if(index > 0){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index-1]] = [updatedTasks[index-1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     function moveTaskDown(index){
-
+        if(index < tasks.length -1){
+            const updatedTasks = [...tasks];
+            [updatedTasks[index], updatedTasks[index+1]] = [updatedTasks[index+1], updatedTasks[index]];
+            setTasks(updatedTasks);
+        }
     }
 
     return(
@@ -48,15 +57,21 @@ function ToDoList(){
             {tasks.map((task, index) => 
                 <li key={index}>
                     <span className="text">{task}</span>
-                    <button className="delete-button"
+                    <button
+                        className="delete-button"
                         onClick={() => deleteTask(index)}>
-                            Delete</button>
-                    <button className="move-button"
+                        Delete
+                    </button>
+                    <button
+                        className="move-button"
                         onClick={() => moveTaskUp(index)}>
-                            Up</button>
-                    <button className="move-button"
+                        ☝
+                    </button>
+                    <button
+                        className="move-button"
                         onClick={() => moveTaskDown(index)}>
-                            Down</button>
+                        👇
+                    </button>
                 </li>
             )}
         </ol>
